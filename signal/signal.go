5add952fbcc4306e0a607fe51cae6ec330de172d
wqf <wqf_525@live.cn>
2017-11-13 11:45:38 +0800

    修改原有signal的错误代码

--- signal/signal.go
+++ signal/signal.go
@@ -2,6 +2,7 @@ package signal
 
 import (
 	"os"
+	"os/signal"
 	"syscall"
 
 	"github.com/wuqifei/server_lib/logs"
@@ -10,6 +11,7 @@ import (
 // InitSignal register signals handler.
 func InitSignal() {
 	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGSTOP, syscall.SIGINT, syscall.SIGHUP)
 	defer close(c)
 	for {
 		s := <-c
--- signal/signal_windows.go
+++ signal/signal_windows.go
@@ -10,6 +10,7 @@ import (
 // InitSignal register signals handler.
 func InitSignal() {
 	chanSig := make(chan os.Signal, 1)
+	defer close(chanSig)
 	signal.Notify(chanSig, os.Interrupt, os.Kill)
 	sig := <-chanSig
 	logs.Emergency("recv kill %s", sig.String())

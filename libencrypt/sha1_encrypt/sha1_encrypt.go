0e698bd3516b973af675bebd15e019ac26236de7
wqf <wqf_525@live.cn>
2018-03-30 16:51:13 +0800

    提交们的

---
+++ libencrypt/sha1_encrypt/sha1_encrypt.go
@@ -0,0 +1,16 @@
+package sha1_encrypt
+
+import (
+	"crypto/sha1"
+)
+
+// sha1加密
+func SHA1(buff []byte) []byte {
+	h := sha1.New()
+	//写入要处理的字节。如果是一个字符串，需要使用[]byte(s) 来强制转换成字节数组。
+	h.Write(buff)
+	//这个用来得到最终的散列值的字符切片。Sum 的参数可以用来都现有的字符切片追加额外的字节切片：一般不需要要。
+	bs := h.Sum(nil)
+	//SHA1 值经常以 16 进制输出，例如在 git commit 中。使用%x 来将散列结果格式化为 16 进制字符串。
+	return bs
+}

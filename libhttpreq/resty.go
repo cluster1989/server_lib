8b62e0d8d153aae8764ff26544b29f9b30d30b53
wqf <wqf_525@live.cn>
2018-07-14 19:03:39 +0800

    删除多余http请求库

--- libhttpreq/resty.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2015-2017 Jeevanandam M (jeeva@myjeeva.com), All rights reserved.
-// resty source code and usage is governed by a MIT style
-// license that can be found in the LICENSE file.
-
-// package libhttpreq provides simple HTTP and REST client for Go inspired by Ruby rest-client.
-package libhttpreq
-
-// Version # of resty
-const Version = "1.1-edge"
